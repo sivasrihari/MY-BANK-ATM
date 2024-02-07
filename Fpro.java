4145d4c46a2ef7698a11889b410ee853bac08e49
sivasrihari <119554131+sivasrihari@users.noreply.github.com>
2024-02-07 16:14:37 +0530

    Add files via upload

---
+++ Fpro.java
@@ -0,0 +1,17 @@
+package projava;
+/*
+ * plese try the credentials as 
+ * 1-(12345678,1234)
+ * 2-(87654321,8765)
+ */
+
+public class Fpro
+{
+    public static void main(String[] args)
+    {
+        Secpro Se=new Secpro();
+        Se.Login();
+    }
+
+}
+
